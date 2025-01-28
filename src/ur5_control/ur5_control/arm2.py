783bc97b65dfb06889cf73afc47ecb5f3475aa18
Pradyumna173 <prponkshe173@gmail.com>
2025-01-28 16:11:52 +0530

    idk slot 3

--- src/ur5_control/ur5_control/arm2.py
+++ src/ur5_control/ur5_control/arm2.py
@@ -5,19 +5,20 @@
 # Team ID:          1048
 # Theme:            Logistic Cobot
 # Author List:      Pradyumna Ponkshe, Shantanu Ekad
-# Filename:         arm_1048.py
-# Functions:        < Comma separated list of functions in this file >
+# Filename:         arm2.py
+# Functions:        
 # Global variables: ARUCO_DICT, ARUCO_PARAMS, detector
 """
 
-import rclpy, sys, cv2, math, time
+import rclpy, math, time, sys
+import cv2
 import tf2_ros
 import numpy as np
 from rclpy.node import Node
 from cv_bridge import CvBridge
 from geometry_msgs.msg import TransformStamped, TwistStamped
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup, ReentrantCallbackGroup
-from sensor_msgs.msg import CompressedImage, Image
+from sensor_msgs.msg import Image
 from tf_transformations import (
     quaternion_from_euler,
     euler_from_quaternion,
@@ -29,7 +30,6 @@ from tf_transformations import (
 from functools import partial
 from std_msgs.msg import Float64
 
-# from linkattacher_msgs.srv import AttachLink, DetachLink
 from ur_msgs.srv import SetIO
 from std_srvs.srv import Trigger
 
@@ -37,43 +37,37 @@ from std_srvs.srv import Trigger
 ARUCO_DICT = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
 ARUCO_PARAMS = cv2.aruco.DetectorParameters()
 detector = cv2.aruco.ArucoDetector(ARUCO_DICT, ARUCO_PARAMS)
+MARKER_SIZE = 0.15
+
+
+def rotation_matrix_to_euler_angles(R):
+    sy = math.sqrt(
+        R[0, 0] ** 2 + R[1, 0] ** 2
+    )  # Calculate the magnitude of the first two elements in the first column of R
+
+    singular = sy < 1e-6  # Check for a singularity (gimbal lock)
+
+    if not singular:
+        roll = math.atan2(R[2, 1], R[2, 2])
+        pitch = math.atan2(-R[2, 0], sy)
+        yaw = math.atan2(R[1, 0], R[0, 0])
+    else:  # Handle singularity where pitch is limited to +-90 degrees
+        roll = math.atan2(-R[1, 2], R[1, 1])
+        pitch = math.atan2(-R[2, 0], sy)
+        yaw = 0
+
+    return (
+        np.degrees(roll),
+        np.degrees(pitch),
+        np.degrees(yaw),
+    )  # Converting to degrees for debugging. It is easier to imagine degrees than radian.
 
 
 def detect_aruco(image):
-    """
-    Purpose:
-    ---
-    Detect ArUco markers in an input image and estimate their pose (translation and rotation)
-    using a predefined dictionary.
-
-    Input Arguments:
-    ---
-    `image` : [ numpy.ndarray ]
-        The input image, gotten from aruco_tf.color_cam_sub in which ArUco markers are to be
-        detected.
-
-    Returns:
-    ---
-    `translation_aruco_list` : [ list ]
-        A list of translations (x, y, z) for each detected ArUco marker.
-
-    `angle_aruco_list` : [ list ]
-        A list of angles (roll, pitch, yaw) for each detected ArUco marker, in radians.
-
-    `marker_ids` : [ list ]
-        A list of IDs corresponding to the detected ArUco markers.
-
-    Example call:
-    ---
-    translation_aruco_list, angle_aruco_list, marker_ids = detect_aruco(input_image)
-    """
-
-    # Empty Arrays made, to fill return at end of function with meaningful values
     translation_aruco_list = []
     angle_aruco_list = []
     marker_ids = []
 
-    # Distortion matrix provided by E-Yantra team for their particular camera/camera-plugin
     camera_matrix = np.array(
         [
             [931.1829833984375, 0.0, 640.0],
@@ -90,11 +84,6 @@ def detect_aruco(image):
 
     if ids is not None:
         for corner, marker_id in zip(corners, ids):
-            if marker_id == 6:  # Marker ID Sim is 12. In Hardware it is 6
-                MARKER_SIZE = 0.15  # Sim Aruco Size = 0.1275m. Hardware Aruco = 0.15
-            else:
-                MARKER_SIZE = 0.15
-
             rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(
                 corner, MARKER_SIZE, camera_matrix, dist_coeffs
             )
@@ -119,81 +108,23 @@ def detect_aruco(image):
             translation_aruco_list.append(translation_aruco)
             angle_aruco_list.append(angle_aruco)
 
-            print(f"Marker ID: {marker_id[0]}")
-            print(f"Translation (x, y, z): {tvec[0][0]}")
             print(
-                f"Rotation (roll, pitch, yaw): {roll:.2f}, {pitch:.2f}, {yaw:.2f}\n"
-            )  # Printing in degrees for debugging
+                f"Marker ID: {marker_id[0]}, Translation (x, y, z): ({tvec[0][0][0]:.2f}, {tvec[0][0][1]:.2f}, {tvec[0][0][2]:.2f})"
+            )
 
-    # cv2.imshow("ArUco Marker Detection", image)
-    # cv2.waitKey(1)
-
-    return translation_aruco_list, angle_aruco_list, marker_ids
-
-
-def rotation_matrix_to_euler_angles(R):
-    """
-    Purpose:
-    ---
-    Convert a rotation matrix to Euler angles (roll, pitch, yaw).
-
-    Input Arguments:
-    ---
-    `R` : [ numpy.ndarray ]
-        A 3x3 rotation matrix representing the rotation.
-
-    Returns:
-    ---
-    `roll` : [ float ]
-        The roll angle in degrees.
-
-    `pitch` : [ float ]
-        The pitch angle in degrees.
-
-    `yaw` : [ float ]
-        The yaw angle in degrees.
-
-    Example call:
-    ---
-    roll, pitch, yaw = rotation_matrix_to_euler_angles(rotation_matrix)
-    """
-
-    sy = math.sqrt(
-        R[0, 0] ** 2 + R[1, 0] ** 2
-    )  # Calculate the magnitude of the first two elements in the first column of R
-
-    singular = sy < 1e-6  # Check for a singularity (gimbal lock)
-
-    if not singular:
-        roll = math.atan2(R[2, 1], R[2, 2])
-        pitch = math.atan2(-R[2, 0], sy)
-        yaw = math.atan2(R[1, 0], R[0, 0])
-    else:  # Handle singularity where pitch is limited to +-90 degrees
-        roll = math.atan2(-R[1, 2], R[1, 1])
-        pitch = math.atan2(-R[2, 0], sy)
-        yaw = 0
-
-    return (
-        np.degrees(roll),
-        np.degrees(pitch),
-        np.degrees(yaw),
-    )  # Converting to degrees for debugging. It is easier to imagine degrees than radian.
+            return translation_aruco_list, angle_aruco_list, marker_ids
 
 
 class ArucoTF(Node):
     def __init__(self):
-
         super().__init__("aruco_tf_node")
-        # Variables
         self.ur5_engaged = False
-        self.current_box = None
+
         self.servo_started = False
         self.servo_x = None
         self.servo_y = None
         self.servo_z = None
-
         self.temp_z = None
-
         self.servo_roll = None
         self.servo_pitch = None
         self.servo_yaw = None
@@ -209,12 +140,11 @@ class ArucoTF(Node):
             1.56192393,
         ]  # Changes when cam sees ebot aruco
 
-        # Callback group declaration
+        # Callback Groups
         self.sub_group = ReentrantCallbackGroup()
         self.client_group = MutuallyExclusiveCallbackGroup()
         self.controller_group = MutuallyExclusiveCallbackGroup()
         self.image_group = MutuallyExclusiveCallbackGroup()
-        # Server groups
         self.passing_group = MutuallyExclusiveCallbackGroup()
 
         # Subscribers
@@ -232,25 +162,11 @@ class ArucoTF(Node):
             "/net_wrench",
             self.force_sub_callback,
             10,
-            callback_group=self.sub_group
-        )
-
-        self.depth_cam_sub = self.create_subscription(
-            Image,
-            "/camera/aligned_depth_to_color/image_raw",
-            self.depthimagecb,
-            10,
             callback_group=self.sub_group,
-        )  # Not in use right now
-
-        # Publishers
-        # self.servo_pub = self.create_publisher(
-        #     TwistStamped, "/servo_node/delta_twist_cmds", 10
-        # )
-
-        self.servo_pub = self.create_publisher(
-            TwistStamped, "/ServoCmdVel", 10
         )
+
+        # Publisher
+        self.servo_pub = self.create_publisher(TwistStamped, "/ServoCmdVel", 10)
 
         # Timers
         self.process_image_timer = self.create_timer(
@@ -275,49 +191,16 @@ class ArucoTF(Node):
 
         # Cv2 Objects
         self.bridge = CvBridge()
-        self.force = None
 
         self.call_servo_trigger()
 
-    # ---------------------------------------- CLASS END ------------------------------------------
-    def force_sub_callback(self, msg):
-        self.force = msg.data
-        #print("wrench", self.force)
-
+    #                                       --- VARIABLES END ---
 
     def pick_box(self):
-        """
-        Purpose:
-        ---
-        Guides the UR5 end-effector to pick up a specified box or the first available box
-        in the `box_dict` as added by process_image function. The function aligns the
-        end-effector's orientation and position with the box's coordinates, attaches the box,
-        and confirms the pick operation.
-
-        Input Arguments:
-        ---
-        `box_number` : [ int or str | optional | default=None ]
-            Identifier for the box to be picked. If not provided, the first box in `box_dict` is
-            chosen.
-
-        Returns:
-        ---
-        `box_number` : [ int or str ]
-            Identifier of the box that was successfully picked.
-
-        Example call:
-        ---
-        pick_box(69)  # Pick the box with ID 69.
-        picked_box = pick_box()  # Pick the first available box.
-        """
-        print("wrench", self.force)
         if self.ur5_engaged:
-            
             return
 
-        if self.image_received:
-            self.process_image()
-        else:
+        if not self.image_received:
             return
 
         if not self.box_dict:
@@ -330,12 +213,9 @@ class ArucoTF(Node):
         box_number = list(self.box_dict)[0]
         box_pose = self.box_dict[box_number]
 
-        print(box_number)
-
         goal_x, goal_y, goal_z = box_pose
-        goal_x += 0.08
-        goal_z -= 0.05
-        self.temp_z = goal_z + 0.05
+        # goal_x += 0.08
+        self.temp_z = goal_z
 
         goal_rot = math.pi
 
@@ -346,51 +226,59 @@ class ArucoTF(Node):
             else -goal_rot - self.servo_roll
         )
 
+        error_x = goal_x - self.servo_x
+        error_y = goal_y - self.servo_y
+
         twist_msg = TwistStamped()
         twist_msg.header.frame_id = "base_link"
+
         while abs(rot_error) > 0.05:
             rot_error = (
                 goal_rot - abs(self.servo_roll)
                 if self.servo_roll >= 0
                 else -goal_rot - self.servo_roll
             )
+
+            error_x = goal_x - self.servo_x
+            error_y = goal_y - self.servo_y
+
             twist_msg.header.stamp = self.get_clock().now().to_msg()
+
+            twist_msg.twist.linear.y = 10.0 * error_y
+            twist_msg.twist.linear.x = 0.0
+            if abs(error_y) < 0.02:
+                twist_msg.twist.linear.x = 10.0 * error_x
+
             twist_msg.twist.angular.y = 15.0 * rot_error
             self.servo_pub.publish(twist_msg)
             time.sleep(0.05)
 
-        twist_msg = TwistStamped()
-        twist_msg.header.frame_id = "base_link"
-
-        error_x = goal_x - self.servo_x
-        error_y = goal_y - self.servo_y
-        error_z = goal_z - self.servo_z
-
-        while (abs(error_x) > 0.02) or (abs(error_y) > 0.02) or (abs(error_z) > 0.02):
+        twist_msg.twist.angular.y = 0.0
+        
+        while (abs(error_x) > 0.02) or (abs(error_y) > 0.02) or (self.force < 90.0):
             error_x = goal_x - self.servo_x
             error_y = goal_y - self.servo_y
-            error_z = goal_z - self.servo_z
-            if self.force > 90.0:
-                error_x = 0.0
-                error_y = 0.0
-                error_z = 0.0
-                print("NET FORCE VAADHLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+            
             twist_msg.header.stamp = self.get_clock().now().to_msg()
 
             twist_msg.twist.linear.y = 10.0 * error_y
-            #            twist_msg.twist.linear.z = 10.0 * error_z
+            #twist_msg.twist.linear.z = 10.0 * error_z
             twist_msg.twist.linear.x = 0.0
-            twist_msg.twist.linear.z = 0.0
+            
 
             if abs(error_y) < 0.02:
                 twist_msg.twist.linear.x = 10.0 * error_x
                 if abs(error_x) < 0.02:
-                    twist_msg.twist.linear.z = 10.0 * error_z
+                    twist_msg.twist.linear.z = 5.0
+                
 
             self.servo_pub.publish(twist_msg)
-            # print(twist_msg)
 
             time.sleep(0.05)
+
+        twist_msg.twist.linear.x = 0.0
+        twist_msg.twist.linear.y = 0.0
+        twist_msg.twist.linear.z = 0.0
 
         self.picked = False
         self.call_attach_box(box_number)
@@ -401,27 +289,8 @@ class ArucoTF(Node):
         self.pass_box(box_number)
 
     def pass_box(self, box_number):
-        """
-        Purpose:
-        ---
-        Commands the UR5 end-effector to move towards the ebot position for passing a box
-        and detaches the specified box once the position is reached.
 
-        Input Arguments:
-        ---
-        `box_number` : [ int or str ]
-            Identifier for the box to be detached and passed.
-
-        Returns:
-        ---
-        None
-
-        Example call:
-        ---
-        pass_box(69)
-        """
         while not self.ebot_pose:
-            self.process_image()
             pass
 
         goal_x, goal_y, goal_z = self.ebot_pose
@@ -430,13 +299,13 @@ class ArucoTF(Node):
 
         error_x = goal_x - self.servo_x
         error_y = goal_y - self.servo_y
-        error_z = (self.temp_z + 0.2) - self.servo_z
+        error_z = (self.temp_z + 0.1) - self.servo_z
         # error_z = 0.0
 
         while (abs(error_x) > 0.02) or (abs(error_y) > 0.02) or (abs(error_z) > 0.02):
             error_x = goal_x - self.servo_x
             error_y = goal_y - self.servo_y
-            error_z = (self.temp_z + 0.2) - self.servo_z
+            error_z = (self.temp_z + 0.1) - self.servo_z
             twist_msg.header.stamp = self.get_clock().now().to_msg()
 
             twist_msg.twist.linear.z = 10.0 * error_z
@@ -462,26 +331,6 @@ class ArucoTF(Node):
             pass
 
     def servo_lookup(self):
-        """
-        Purpose:
-        ---
-        Retrieves the transformation between the `base_link` and the end effector of the UR5 arm,
-        extracting the translation and orientation (as roll, pitch, and yaw). Updates the
-        object's attributes with the transformation values.
-
-        Input Arguments:
-        ---
-        None
-
-        Returns:
-        ---
-        None
-
-        Example call:
-        ---
-        servo_lookup()
-        """
-
         try:
             self.servo_transform = self.tf_buffer.lookup_transform(
                 "base_link", "wrist_3_link", rclpy.time.Time()
@@ -517,26 +366,6 @@ class ArucoTF(Node):
             )
 
     def process_image(self):
-        """
-        Purpose:
-        ---
-        Process an input image to detect ArUco markers, broadcast transformations for each
-        detected marker, and update the ebot's pose or box dictionary. Also removes boxes that
-        have already been transferred from box dictionary
-
-        Input Arguments:
-        ---
-        None
-
-        Returns:
-        ---
-        None
-
-        Example call:
-        ---
-        Timer based call
-        """
-
         if not self.image_received:
             return
 
@@ -551,15 +380,17 @@ class ArucoTF(Node):
                 -translation[i][0],
                 -translation[i][1],
             ]
+
             self.broadcast_transform(
                 # "camera_color_optical_frame",
-                "camera_color_optical_frame",
+                "camera_link",
                 "1048_cam_" + str(ids[i]),
-                translation[i],
+                rotated_translation,
                 quaternion_from_euler(0.0, 0.0, 0.0, "sxyz"),
             )  # Broadcast aruco tf w.r.t camera
+
             base_to_aruco_pose = self.find_transform(
-                translation[i],
+                rotated_translation,
                 quaternion_from_euler(angle[i][0], angle[i][1], angle[i][2], "sxyz"),
             )  # Lookup base to camera transform and find base to box transform
 
@@ -587,36 +418,10 @@ class ArucoTF(Node):
         return
 
     def find_transform(self, translation, angles):
-        """
-        Purpose:
-        ---
-        Calculate the transformation from the given tf to UR5 base_link frame, using the
-        translation and rotation (angles) of the ArUco marker relative to the camera.
-
-        Input Arguments:
-        ---
-        `translation` : [ list ]
-            A list of three values representing the translation (x, y, z) of the ArUco marker
-            relative to the camera.
-
-        `angles` : [ list ]
-            A list of four values representing the rotation (quaternion) of the ArUco marker
-            relative to the camera.
-
-        Returns:
-        ---
-        `aruco_translation_in_base` : [ list ]
-            The calculated translation of the ArUco marker in the base_link frame.
-
-        Example call:
-        ---
-        aruco_translation_in_base = find_transform(aruco_translation, aruco_angles)
-        """
-
         try:
 
             base_to_camera = self.tf_buffer.lookup_transform(
-                "base_link", "camera_color_optical_frame", rclpy.time.Time()
+                "base_link", "camera_link", rclpy.time.Time()
             )
 
             base_translation = [
@@ -656,37 +461,6 @@ class ArucoTF(Node):
             self.get_logger().info(f"Could not calculate transform: {e}")
 
     def broadcast_transform(self, parent_frame, child_frame, translation, rotation):
-        """
-        Purpose:
-        ---
-        Broadcast a transformation between two frames (parent and child) with a specified
-        translation and rotation(quaternion).
-
-        Input Arguments:
-        ---
-        `parent_frame` : [ str ]
-            The name of the parent frame in the transformation.
-
-        `child_frame` : [ str ]
-            The name of the child frame in the transformation.
-
-        `translation` : [ list ]
-            A list of three values representing the translation (x, y, z) from the parent frame
-            to the child frame.
-
-        `rotation` : [ list ]
-            A list of four values representing the rotation (quaternion) from the parent frame
-            to the child frame.
-
-        Returns:
-        ---
-        None
-
-        Example call:
-        ---
-        broadcast_transform("base_link", "1048_base_69", [69.0, 42.0, 0.0], [0.0, 0.0, 0.0, 1.0])
-        """
-
         transform = TransformStamped()
         transform.header.stamp = self.get_clock().now().to_msg()
         transform.header.frame_id = parent_frame
@@ -703,6 +477,7 @@ class ArucoTF(Node):
 
         self.tf_broadcaster.sendTransform(transform)
 
+    # Client Functions
     # def call_attach_box(self, box_number):
     #     client = self.create_client(AttachLink, "GripperMagnetON")
     #     while not client.wait_for_service(timeout_sec=1.0):
@@ -808,12 +583,14 @@ class ArucoTF(Node):
     def colorimagecb(self, data):
         self.cv_image = self.bridge.imgmsg_to_cv2(data, desired_encoding="bgr8")
         self.image_received = True
-        # print("Image Received")
 
     def depthimagecb(self, data):
         self.depth_image = self.bridge.imgmsg_to_cv2(
             data, desired_encoding="passthrough"
         )
+
+    def force_sub_callback(self, msg):
+        self.force = msg.data
 
 
 def main():
